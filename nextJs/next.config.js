b5422901fade1746d5830bedb262efb962aff9c5
a39955720 <a39955720@gmail.com>
2023-12-10 03:29:00 +0800

    Update config

--- nextJs/next.config.js
+++ nextJs/next.config.js
@@ -3,6 +3,7 @@ dotenv.config()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     reactStrictMode: true,
     env: {
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
