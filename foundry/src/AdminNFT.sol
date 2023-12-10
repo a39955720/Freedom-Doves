e01a5f2e286250c39a0ed7be8ad5916312c44ad7
a39955720 <a39955720@gmail.com>
2023-12-10 18:15:29 +0800

    Add annotation

--- foundry/src/AdminNFT.sol
+++ foundry/src/AdminNFT.sol
@@ -1,46 +1,89 @@
 // SPDX-License-Identifier: MIT
+/**
+ * @title AdminNFT
+ * @dev An ERC721 token contract with additional administrative features.
+ */
 pragma solidity ^0.8.21;
 
+// Importing ERC721 and IERC721 from OpenZeppelin contracts.
 import {ERC721, IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// Custom error messages for the AdminNFT contract.
 error AdminNFT_YouCantCallThisFunction();
 error AdminNFT_YouCantTransferNFT();
 error AdminNFT_NotOwner();
 
+/**
+ * @notice AdminNFT contract - Extends ERC721 token functionality.
+ */
 contract AdminNFT is ERC721 {
+    // Private variable to store the owner's address.
     address private i_owner;
 
+    // Private variable to store the token counter for unique token IDs.
     uint256 private s_tokenCounter;
+
+    // Private variable to store the address of the FreedomDoves contract.
     address private s_freedomDovesAddr;
 
+    /**
+     * @notice Constructor to initialize the AdminNFT contract.
+     */
     constructor() ERC721("Admin NFT", "AN") {
+        // Set the contract deployer's address as the owner.
         i_owner = msg.sender;
+
+        // Initialize the token counter.
         s_tokenCounter = 0;
     }
 
+    /**
+     * @notice Mint a new NFT and assign it to the specified receiver.
+     * @param receiver The address to receive the newly minted NFT.
+     */
     function mintNft(address receiver) public {
+        // Check if the caller is the FreedomDoves contract.
         if (msg.sender != s_freedomDovesAddr) {
             revert AdminNFT_YouCantCallThisFunction();
         }
+
+        // Mint a new NFT using the internal _mint function.
         _mint(receiver, s_tokenCounter);
+
+        // Increment the token counter to ensure each NFT has a unique ID.
         s_tokenCounter = s_tokenCounter + 1;
     }
 
+    /**
+     * @notice Override transferFrom to prohibit NFT transfers.
+     */
     function transferFrom(
         address /*from*/,
         address /*to*/,
         uint256 /*tokenId*/
     ) public virtual override(ERC721) {
+        // Revert with a custom error message indicating the prohibition of NFT transfers.
         revert AdminNFT_YouCantTransferNFT();
     }
 
+    /**
+     * @notice Set the address of the FreedomDoves contract (admin function).
+     * @param freedomDovesAddr The new address for the FreedomDoves contract.
+     */
     function setFreedomDovesAddr(address freedomDovesAddr) public {
+        // Check if the caller is the contract owner.
         if (msg.sender != i_owner) {
             revert AdminNFT_NotOwner();
         }
+
+        // Set the address of the FreedomDoves contract.
         s_freedomDovesAddr = freedomDovesAddr;
     }
 
+    /**
+     * @notice Get the total supply of NFTs minted by the contract.
+     * @return The total number of NFTs minted.
+     */
     function getTotalSupply() public view returns (uint256) {
         return s_tokenCounter;
     }
--- foundry/src/FreedomDoves.sol
+++ foundry/src/FreedomDoves.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
+/**
+ * @title FreedomDoves
+ * @dev A decentralized forum centered around the themes of 'freedom' and 'peace'.
+ */
 pragma solidity ^0.8.21;
 
+// Importing external contracts.
 import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AdminNFT} from "./AdminNFT.sol";
 
+// Custom error messages for the FreedomDoves contract.
 error FreedomDoves__UpkeepNotNeeded();
 error FreedomDoves_YouHaveAlreadyLikedThisPost();
 error FreedomDoves_YouHaveAlreadyVotedThisPost();
@@ -14,12 +20,17 @@ error FreedomDoves_YouDontHaveAdminNFT();
 error FreedomDoves_ThisPostHasBeenDeleted();
 error FreedomDoves_NotOwner();
 
+/**
+ * @notice FreedomDoves contract - Handles posts, comments, likes, and charitable activities.
+ */
 contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
+    // Enumeration for the state of the controller.
     enum ControllerState {
         OPEN,
         CALCULATING
     }
 
+    // Struct to store post data.
     struct PostData {
         bytes title;
         bytes content;
@@ -29,31 +40,33 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         uint256 numberOfLikes;
     }
 
+    // Struct to store comment data.
     struct CommentData {
         bytes content;
         address commentAuthors;
     }
 
+    // Chainlink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
     uint32 private immutable i_callbackGasLimit;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 3;
-
     bytes private constant DELETEDTEXT =
         hex"0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001b5468697320706f737420686173206265656e2064656c65746564210000000000";
 
+    // Immutable variables.
     IERC20 private immutable i_fusdc;
     AdminNFT private immutable i_adminNFT;
     uint256 private immutable i_interval;
     address private immutable i_owner;
 
+    // State variables.
     ControllerState private s_controllerState;
     uint256 private s_lastTimeStamp;
     uint256 private s_postIdCounter;
     uint256[] private s_topThreePostId = new uint256[](3);
-
     mapping(uint256 => uint256) private s_commentIdCounter;
     mapping(uint256 => PostData) private s_storePost;
     mapping(uint256 => mapping(uint256 => CommentData)) private s_storeComment;
@@ -62,8 +75,19 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
     mapping(uint256 => bool) private s_isDeleted;
     mapping(uint256 => uint256) private s_numberOfLikesThisWeek;
 
+    // Event for requesting random numbers.
     event RequestedRandomNum(uint256 indexed requestId);
 
+    /**
+     * @notice Constructor to initialize the FreedomDoves contract.
+     * @param updateInterval The execution interval of Chainlink Automation.
+     * @param fusdcAddr The address of the FUSDC token contract.
+     * @param adminNFTAddr The address of the AdminNFT contract.
+     * @param vrfCoordinatorV2 The address of the VRFCoordinatorV2 contract.
+     * @param subscriptionId The subscription ID for Chainlink VRF.
+     * @param gasLane The gas lane for Chainlink VRF.
+     * @param callbackGasLimit The callback gas limit for Chainlink VRF.
+     */
     constructor(
         uint256 updateInterval,
         address fusdcAddr,
@@ -86,6 +110,11 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         i_owner = msg.sender;
     }
 
+    /**
+     * @notice Create a new post.
+     * @param _title The title of the post.
+     * @param _content The content of the post.
+     */
     function newPost(bytes memory _title, bytes memory _content) public {
         s_storePost[s_postIdCounter] = PostData(
             _title,
@@ -98,6 +127,11 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         s_postIdCounter++;
     }
 
+    /**
+     * @notice Add a new comment to a post.
+     * @param postId The ID of the post to comment on.
+     * @param _content The content of the comment.
+     */
     function newComment(uint256 postId, bytes memory _content) public {
         if (s_isDeleted[postId] == true) {
             revert FreedomDoves_ThisPostHasBeenDeleted();
@@ -107,6 +141,10 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         s_commentIdCounter[postId]++;
     }
 
+    /**
+     * @notice Like a post.
+     * @param postId The ID of the post to like.
+     */
     function likePost(uint256 postId) public {
         if (s_isLiked[msg.sender][postId] == true) {
             revert FreedomDoves_YouHaveAlreadyLikedThisPost();
@@ -128,6 +166,10 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         }
     }
 
+    /**
+     * @notice Delete a post.
+     * @param postId The ID of the post to delete.
+     */
     function deletePost(uint256 postId) public {
         if (i_adminNFT.balanceOf(msg.sender) <= 0) {
             revert FreedomDoves_YouDontHaveAdminNFT();
@@ -150,6 +192,9 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         }
     }
 
+    /**
+     * @notice Set the controller state to OPEN (owner-only function).
+     */
     function setControllerState() public {
         if (msg.sender != i_owner) {
             revert FreedomDoves_NotOwner();
@@ -157,6 +202,12 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         s_controllerState = ControllerState.OPEN;
     }
 
+    /**
+     * @notice Check if upkeep is needed.
+     * param checkData Additional data for checking upkeep.
+     * @return upkeepNeeded Whether upkeep is needed.
+     * @return performData Data for performing upkeep.
+     */
     function checkUpkeep(
         bytes memory /* checkData */
     )
@@ -173,16 +224,20 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         return (upkeepNeeded, "0x0");
     }
 
+    //For test
     function checkUpkeepp(
         bytes memory /* checkData */
-    ) public view returns (bool, bool, bool) {
-        bool isOpen = s_controllerState == ControllerState.OPEN;
-        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
-        bool hasWinner = s_storePost[s_topThreePostId[0]].postAuthors !=
-            address(0);
+    ) public view returns (bool isOpen, bool timePassed, bool hasWinner) {
+        isOpen = s_controllerState == ControllerState.OPEN;
+        timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        hasWinner = s_storePost[s_topThreePostId[0]].postAuthors != address(0);
         return (isOpen, timePassed, hasWinner);
     }
 
+    /**
+     * @notice Perform upkeep by requesting random numbers.
+     * param performData Data for performing upkeep.
+     */
     function performUpkeep(bytes calldata /* performData */) external override {
         (bool upkeepNeeded, ) = checkUpkeep("");
         if (!upkeepNeeded) {
@@ -199,6 +254,11 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         emit RequestedRandomNum(requestId);
     }
 
+    /**
+     * @notice Fulfillment function for the requested random numbers.
+     * param requestId The ID of the request.
+     * @param randomWords An array of random numbers.
+     */
     function fulfillRandomWords(
         uint256 /* requestId*/,
         uint256[] memory randomWords
@@ -225,14 +285,29 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         s_lastTimeStamp = block.timestamp;
     }
 
+    /**
+     * @notice Get the data of a specific post.
+     * @param postId The ID of the post.
+     * @return The data of the specified post.
+     */
     function getPostData(uint256 postId) public view returns (PostData memory) {
         return s_storePost[postId];
     }
 
+    /**
+     * @notice Get the total number of posts.
+     * @return The total number of posts.
+     */
     function getTotalPost() public view returns (uint256) {
         return s_postIdCounter;
     }
 
+    /**
+     * @notice Get the data of a specific comment.
+     * @param postId The ID of the post.
+     * @param commentId The ID of the comment.
+     * @return The data of the specified comment.
+     */
     function getCommentData(
         uint256 postId,
         uint256 commentId
@@ -240,10 +315,21 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         return s_storeComment[postId][commentId];
     }
 
+    /**
+     * @notice Get the total number of comments for a specific post.
+     * @param postId The ID of the post.
+     * @return The total number of comments for the specified post.
+     */
     function getTotalComment(uint256 postId) public view returns (uint256) {
         return s_commentIdCounter[postId];
     }
 
+    /**
+     * @notice Check if a user has liked a specific post.
+     * @param msgsender The address of the user.
+     * @param postId The ID of the post.
+     * @return Whether the user has liked the post.
+     */
     function getIsLiked(
         address msgsender,
         uint256 postId
@@ -251,10 +337,21 @@ contract FreedomDoves is AutomationCompatibleInterface, VRFConsumerBaseV2 {
         return s_isLiked[msgsender][postId];
     }
 
+    /**
+     * @notice Check if a post has been deleted.
+     * @param postId The ID of the post.
+     * @return Whether the post has been deleted.
+     */
     function getIsDeleted(uint256 postId) public view returns (bool) {
         return s_isDeleted[postId];
     }
 
+    /**
+     * @notice Check if a user can vote on a specific post.
+     * @param msgsender The address of the user.
+     * @param postId The ID of the post.
+     * @return Whether the user can vote on the post.
+     */
     function getCanVote(
         address msgsender,
         uint256 postId
